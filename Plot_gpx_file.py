7a14bf58f12b005113d4a9ffba64fa83934dc9ba
fdlogic <federicogd2009@gmail.com>
2023-08-21 20:46:57 -0300

    Plot gpx file

---
+++ Plot_gpx_file.py
@@ -0,0 +1,69 @@
+from gpx_converter import Converter
+import matplotlib.pyplot as plt 
+import os
+import argparse
+
+
+def convert_gpx_to_dataframe(gpx_file):
+    """
+    This function convert the gpx data to a dataframe.
+
+    Arguments:
+    gpx_file: file with gpx data.
+
+    Return:
+    dataframe: dataframe with the .gpx file data
+    """
+
+    dataframe = Converter(input_file=gpx_file).gpx_to_dataframe(lats_colname='latitude', longs_colname='longitude', times_colname='time', alts_colname='altitude')
+
+    return dataframe
+
+def plot_dataframe(df):
+    """
+    Plot latitude and longitude from the dataframe
+
+    Arguments:
+    df: dataframe with the .gpx file data
+
+    Plot latitude vs longitude
+    """
+
+    #Extract data
+    longitude = df.longitude
+    latitude = df.latitude
+
+    #Plot
+    plt.scatter(longitude, latitude)
+    plt.plot(longitude, latitude, 'r--')
+ 
+    # Labels
+    plt.title('Longitude vs Latitude')
+    plt.xlabel('Latitude')
+    plt.ylabel('Longitude')
+
+    plt.grid()
+    plt.show()
+
+def get_comline_parser():
+    parser = argparse.ArgumentParser(description="Search files")
+    parser.add_argument(
+        "--path", type=str, default=".", help="Path where found the files"
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_comline_parser().parse_args()
+
+    for file_ in os.listdir(args.path):
+        name, extension = os.path.splitext(file_)
+
+        if extension in [".gpx"]:
+            gpx_file = name + extension
+            print("I found the file {}".format(gpx_file))
+
+            dataframe = convert_gpx_to_dataframe(gpx_file)
+           
+            plot_dataframe(dataframe)
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Plot_gpx_file
 This repository plot data in .gpx file
+
+### Prerequisites 
+For this example, use the next requirements: 
+
+```
+pip3 install -r requirements.txt
+```
+
+## Usage
+
+```
+usage: Plot_gpx_file.py [-h] [--path PATH]
+
+Search files
+
+options:
+  -h, --help   show this help message and exit
+  --path PATH  Path where found the files
+```
+
+### Example for convert
+
+```
+python3 Plot_gpx_file.py
+```
+
+## Latitude
+Horizontal mapping lines on Earth are lines of latitude
+
+## Longitude
+Vertical mapping lines on Earth are lines of longitude
